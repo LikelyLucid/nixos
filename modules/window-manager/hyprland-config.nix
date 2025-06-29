0078deeb273e0711fcc110b9e09ef8145c09abad
Michael Mckellar <67492333+LikelyLucid@users.noreply.github.com>
2025-06-29 21:47:12 +1200

    Enable hyprlock and hypridle

--- modules/window-manager/hyprland-config.nix
+++ modules/window-manager/hyprland-config.nix
@@ -13,4 +13,26 @@
 
   services.playerctld.enable = true;
 
+  programs.hyprlock.enable = true;
+  services.hypridle = {
+    enable = true;
+    settings = {
+      general = {
+        lock_cmd = "hyprlock";
+        before_sleep_cmd = "hyprlock";
+      };
+
+      listener = [
+        {
+          timeout = 300;
+          on-timeout = "hyprlock";
+        }
+        {
+          timeout = 600;
+          on-timeout = "systemctl suspend";
+        }
+      ];
+    };
+  };
+
 }
