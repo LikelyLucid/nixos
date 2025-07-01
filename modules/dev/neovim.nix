3ae0da7b130add266ec7b0826140ceca3dfa023f
LikelyLucid <micoolplays@gmail.com>
2025-07-01 18:29:28 +1200

    comit

--- modules/dev/neovim.nix
+++ modules/dev/neovim.nix
@@ -1,11 +1,11 @@
 { pkgs, lazyvim-config, lib, ... }:
 {
   home.packages = with pkgs; [ neovim zig ripgrep gnugrep fzf uv nodejs-slim_20 yazi];
-  home.activation.copyNvimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
-  rm -rf ~/.config/nvim
-  cp -r ${lazyvim-config} ~/.config/nvim
-  chmod -R u+w ~/.config/nvim
-  '';
+  # home.activation.copyNvimConfig = lib.hm.dag.entryAfter ["writeBoundary"] ''
+  # rm -rf ~/.config/nvim
+  # cp -r ${lazyvim-config} ~/.config/nvim
+  # chmod -R u+w ~/.config/nvim
+  # '';
 
   home.sessionVariables = {
     EDITOR = "nvim";
