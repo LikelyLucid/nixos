b367a4677145b68b688bb14b44510c8c3ded4796
LikelyLucid <micoolplays@gmail.com>
2025-07-30 12:25:25 +1200

    fix obsidian scaling issue

--- modules/university/r.nix
+++ modules/university/r.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs; [
-    rstudioWrapper
-    (rWrapper.override {
+  environment.systemPackages = with pkgs;
+  let
+    RStudio-with-my-packages = rstudioWrapper.override {
       packages = with rPackages; [
-        tidyverse
-        ggplot2
+        tidyverse # ggplot2 is part of tidyverse
       ];
-    })
+    };
+  in [
+    RStudio-with-my-packages
   ];
 }
