2b476f12ddf1f505ce10cb6fc0b4b352102d1d4a
LikelyLucid <micoolplays@gmail.com>
2025-08-06 16:28:42 +1200

    fix scaling issues

--- modules/office/libreoffice.nix
+++ modules/office/libreoffice.nix
@@ -2,4 +2,8 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [ libreoffice ];
+
+  home.sessionVariables = {
+    SAL_USE_VCLPLUGIN = "gtk3";
+  };
 }
--- modules/university/r.nix
+++ modules/university/r.nix
@@ -1,14 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  environment.systemPackages = with pkgs;
+  home.packages = with pkgs;
   let
     RStudio-with-my-packages = rstudioWrapper.override {
       packages = with rPackages; [
-        tidyverse performance readr kableExtra see readxl
+        tidyverse performance readr kableExtra see readxl stringi GGally
       ];
     };
   in [
     RStudio-with-my-packages
   ];
+
+  home.sessionVariables = {
+    QT_QPA_PLATFORM = "wayland";
+    QT_SCALE_FACTOR = "1";
+  };
 }
