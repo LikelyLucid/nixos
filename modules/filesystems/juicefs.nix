db47269f29963c5b503402f91f4421b379745dbb
LikelyLucid <micoolplays@gmail.com>
2026-07-18 15:32:08 +1200

    feat: add encrypted JuiceFS mount

---
+++ modules/filesystems/juicefs.nix
@@ -0,0 +1,87 @@
+{ ... }:
+{
+  nixos.modules.artsxps =
+    {
+      config,
+      lib,
+      pkgs,
+      ...
+    }:
+    let
+      juicefs = pkgs.stdenvNoCC.mkDerivation {
+        pname = "juicefs";
+        version = "1.4.0";
+
+        src = pkgs.fetchurl {
+          url = "https://github.com/juicedata/juicefs/releases/download/v1.4.0/juicefs-1.4.0-linux-amd64.tar.gz";
+          hash = "sha256-be3XMEh+fawbEcWAFoKolpLy5rl4kLr3rJQ0B1ALhas=";
+        };
+
+        dontUnpack = true;
+        installPhase = ''
+          runHook preInstall
+
+          tar -xzf "$src"
+          install -Dm755 juicefs "$out/bin/juicefs"
+          ln -s juicefs "$out/bin/mount.juicefs"
+
+          runHook postInstall
+        '';
+
+        meta = {
+          description = "Distributed POSIX file system built on top of Redis and S3";
+          homepage = "https://www.juicefs.com/";
+          license = lib.licenses.asl20;
+          mainProgram = "juicefs";
+          platforms = [ "x86_64-linux" ];
+        };
+      };
+
+      mount_juicefs = pkgs.writeShellApplication {
+        name = "mount-juicefs";
+        text = ''
+          META_PASSWORD="$(<${config.sops.secrets.juicefs-meta-password.path})"
+          export META_PASSWORD
+          exec ${juicefs}/bin/juicefs mount \
+            redis://redis.likelylucid.com:6379/2 \
+            /mnt/juicefs \
+            --cache-dir /var/cache/juicefs \
+            -o allow_other
+        '';
+      };
+    in
+    {
+      environment.systemPackages = [ juicefs ];
+
+      programs.fuse.userAllowOther = true;
+
+      sops.secrets.juicefs-meta-password = {
+        mode = "0400";
+        restartUnits = [ "juicefs.service" ];
+      };
+
+      systemd.tmpfiles.rules = [ "d /mnt/juicefs 0755 root root -" ];
+
+      systemd.services.juicefs = {
+        description = "JuiceFS mount";
+        wantedBy = [ "multi-user.target" ];
+        requires = [ "sops-install-secrets.service" ];
+        after = [
+          "network-online.target"
+          "sops-install-secrets.service"
+        ];
+        wants = [ "network-online.target" ];
+
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${mount_juicefs}/bin/mount-juicefs";
+          ExecStop = "${juicefs}/bin/juicefs umount /mnt/juicefs";
+          CacheDirectory = "juicefs";
+          CacheDirectoryMode = "0700";
+          Restart = "on-failure";
+          RestartSec = 5;
+          TimeoutStopSec = 60;
+        };
+      };
+    };
+}
