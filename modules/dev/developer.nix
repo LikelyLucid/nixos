4907823a06ad3e19a4f062e184c38a5670fe6414
LikelyLucid <micoolplays@gmail.com>
2026-08-07 10:35:23 +1200

    add tuicr code review tool

--- modules/dev/developer.nix
+++ modules/dev/developer.nix
@@ -1,45 +1,49 @@
 { ... }:
 {
-  homeManager.modules.common = {
-    programs = {
-      ############################################
-      # GIT CONFIG
-      ############################################
-      git = {
-        enable = true;
-        settings = {
-          user.name = "LikelyLucid";
-          user.email = "micoolplays@gmail.com";
-          core.editor = "nvim";
-          diff.colorMoved = "default";
-          fetch.prune = true;
-          merge.conflictStyle = "zdiff3";
-          pull.rebase = true;
-          rerere.enabled = true;
+  homeManager.modules.common =
+    { pkgs, ... }:
+    {
+      home.packages = [ pkgs.tuicr ];
+
+      programs = {
+        ############################################
+        # GIT CONFIG
+        ############################################
+        git = {
+          enable = true;
+          settings = {
+            user.name = "LikelyLucid";
+            user.email = "micoolplays@gmail.com";
+            core.editor = "nvim";
+            diff.colorMoved = "default";
+            fetch.prune = true;
+            merge.conflictStyle = "zdiff3";
+            pull.rebase = true;
+            rerere.enabled = true;
+          };
+        };
+
+        delta = {
+          enable = true;
+          enableGitIntegration = true;
+        };
+
+        bat.enable = true;
+
+        eza = {
+          enable = true;
+          icons = "auto";
+          extraOptions = [ "--group-directories-first" ];
+        };
+
+        fzf = {
+          enable = true;
+          enableZshIntegration = true;
+          defaultCommand = "fd --type f --hidden --follow --exclude .git";
+          fileWidget.command = "fd --type f --hidden --follow --exclude .git";
+          changeDirWidget.command = "fd --type d --hidden --follow --exclude .git";
+          historyWidget.command = "";
         };
       };
-
-      delta = {
-        enable = true;
-        enableGitIntegration = true;
-      };
-
-      bat.enable = true;
-
-      eza = {
-        enable = true;
-        icons = "auto";
-        extraOptions = [ "--group-directories-first" ];
-      };
-
-      fzf = {
-        enable = true;
-        enableZshIntegration = true;
-        defaultCommand = "fd --type f --hidden --follow --exclude .git";
-        fileWidget.command = "fd --type f --hidden --follow --exclude .git";
-        changeDirWidget.command = "fd --type d --hidden --follow --exclude .git";
-        historyWidget.command = "";
-      };
     };
-  };
 }
